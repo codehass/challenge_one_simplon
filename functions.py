36c909d48667443082b63c19ee29b8c5e5af3631
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-09-25 23:02:47 +0100

    add more functions

--- functions.py
+++ functions.py
@@ -1,14 +1,29 @@
+from helpers import luxuryFunction
+
 def display_list(list, text):
+  print("-------------------")
   print(text)
+  print("-------------------")
 
   for item in list:
-    print(item)
+    print(" ",item)
+  print("\n")
 
-def priceSortFunction(e):
-  return e[1]
+def get_products_prices(products, prices):
+    return list(zip(products, prices))
 
-def luxuryFunction(item):
-  if item[1] > 1000:
-    return f"{item[0]} coute {item[1]} DH (LUXE)"
-  else:
-    return f"{item[0]} coute {item[1]} DH"
+def addNewProduct(product_name, product_price,products_list, prices_list):
+  products_list.append(product_name)
+  prices_list.append(product_price)
+
+def search_product_by_name(product_name, products_list):
+  return list(filter(lambda product: product[0] == product_name, products_list))
+
+def filtered_products(list_product: list)-> list:
+  return list(filter(lambda item: item[1] > 30, list_product))
+
+def products_price_string_list (list_product: list)-> list:
+  return list(map(lambda item: f"{item[0]} coute {item[1]} DH", list_product))
+
+def luxury_products(list_product: list)-> list:
+  return list(map(luxuryFunction, list_product))
