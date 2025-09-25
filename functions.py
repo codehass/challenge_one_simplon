734690685ca8ec70b7dd335c4df0974f204c4c01
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-09-25 12:42:49 +0100

    crete function luxuryFunction

--- functions.py
+++ functions.py
@@ -6,3 +6,9 @@ def display_list(list, text):
 
 def priceSortFunction(e):
   return e[1]
+
+def luxuryFunction(item):
+  if item[1] > 1000:
+    return f"{item[0]} coute {item[1]} DH (LUXE)"
+  else:
+    return f"{item[0]} coute {item[1]} DH"
